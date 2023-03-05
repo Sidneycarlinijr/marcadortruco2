a2b95e9350e69a6da181baf6f5ecdb6702ae886a
Sidney Carlini Junior <sidney_carlini@hotmail.com>
2023-03-05 15:00:55 -0300

    home layout design

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -3,7 +3,9 @@ import React, { Component } from 'react';
 import styled from 'styled-components';
 import Clown from '../styles/assets/clown.svg'
 import Spades from '../styles/assets/spades.png'
-import Heart from '../styles/assets/heart.png'
+import Hearts from '../styles/assets/hearts.png'
+import Clubs from '../styles/assets/clubs.png'
+import Diamonds from '../styles/assets/diamonds.png'
 
 const HomeContainer = styled.div`
   display: flex;
@@ -27,21 +29,25 @@ const Top = styled.div`
   display: flex;
   flex-direction: column;
   width: 100%;
-  flex: 1;
+  height: 35%;
+  flex: none;
 `
 const Mid = styled.div`
   flex: 1;
+  position: relative;
+  bottom: 0.3rem;
 `
 const Bot = styled.div`
   display: flex;
   flex-direction: column;
-  flex: 1;
+  flex: none;
+  height: 25%;
   justify-content: center;
-  gap: 15px;
+  gap: 30px;
 `
 const HomeButton = styled.button`
-  width: 18rem;
-  height: 3rem;
+  width: 20rem;
+  height: 3.35rem;
   background-color: #D9B165;
   border: solid 3px #A27946;
   border-radius: 10px;
@@ -69,21 +75,31 @@ class Home extends Component {
                   <Image src={Spades} alt="spades" width={18} height={23}/>
                 </div>
                 <div style={{ display: 'flex', alignItems: 'flex-end', flex: 'none', width: '45%', height: '100%', marginBottom: '0.5rem' }}>
-                  <hr style={{border: '0.5px solid #D9B165', width: '100%'}}>
+                  <hr style={{border: '1px solid #D9B165', width: '100%'}}>
                   </hr>
                 </div>
                 <div style={{ display: 'flex', justifyContent: 'center', flex: 1 }}>
-                  <Image src={Heart} alt="heart" width={20} height={20}/>
+                  <Image src={Hearts} alt="hearts" width={20} height={20}/>
                 </div>
               </TopFirstDiv>
               <div style={{ flex: '1', display: 'flex', marginTop: '2.5rem' }}>
                 <div style={{ flex: '1', display: 'flex', alignItems:'center', justifyContent: 'center', paddingLeft: '2rem' }}>
                   <div style={{ width: '100%', height: '100%', borderTop: '1px solid #D9B165', borderLeft: '1px solid #D9B165' }}></div>
                 </div>
-                <div style={{ flex: 'none', display: 'flex', flexDirection: 'column', alignItems:'center', justifyContent: 'center', width: '5rem' }}>
-                  <div>a</div>
-                  <div>b</div>
-                  <div>c</div>
+                <div style={{ flex: 'none', display: 'flex', flexDirection: 'column', alignItems:'center', justifyContent: 'flex-start', width: '5rem' }}>
+                  <div style={{ flex: 'none', display: 'flex', justifyContent: 'center', height: '2rem', width: '100%', position: 'relative', bottom: '0.29rem'}}>
+                    <div style={{ flex: '1', display: 'flex', justifyContent: 'center', fontSize: '8px', color: '#D9B165' }}>00</div>
+                    <div style={{ flex: '1', display: 'flex', justifyContent: 'center', position: 'relative', bottom: '0.4rem' }}>
+                      <Image src={Clubs} alt="cardOfClubs"/>
+                    </div>
+                    <div style={{ flex: '1', display: 'flex', justifyContent: 'center', fontSize: '8px', color: '#D9B165' }}>12</div>
+                  </div>
+                  <div>
+                    <h2 style={{ fontSize: '3rem', color: '#D9B165', fontWeight: 'normal' }}>TRUCO</h2>
+                  </div>
+                  <div style={{ marginTop: '0.5rem'}}>
+                    <Image src={Diamonds} alt="diamonds"/>
+                  </div>
                 </div>
                 <div style={{ flex: '1', display: 'flex', alignItems:'center', justifyContent: 'center', paddingRight: '2rem' }}>
                   <div style={{ width: '100%', height: '100%', borderTop: '1px solid #D9B165', borderRight: '1px solid #D9B165' }}></div>
@@ -91,7 +107,7 @@ class Home extends Component {
               </div>
             </Top>
             <Mid>
-              <Image width={215} height={250} src={Clown} alt={'centerImg'}/>
+              <Image width={240} height={280} src={Clown} alt={'centerImg'}/>
             </Mid>
             <Bot>
               <HomeButton>
